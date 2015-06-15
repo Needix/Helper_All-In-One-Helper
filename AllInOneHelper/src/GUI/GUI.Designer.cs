3901cb9833b01a988c9564405c03dfec4ff13409
Jonas Oeldemann <joeldem@mail.uni-paderborn.de>
2015-06-15 20:13:00 +0200

    - Created tabs for modules

--- AllInOneHelper/src/GUI/GUI.Designer.cs
+++ AllInOneHelper/src/GUI/GUI.Designer.cs
@@ -23,59 +23,181 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.tabControl_main = new System.Windows.Forms.TabControl();
+            this.main_tabPage_aspectRatio = new System.Windows.Forms.TabPage();
+            this.main_tabPage_bpm = new System.Windows.Forms.TabPage();
+            this.main_tabPage_clickSpeed = new System.Windows.Forms.TabPage();
+            this.main_tabPage_clipboard = new System.Windows.Forms.TabPage();
+            this.main_tabPage_copyFinder = new System.Windows.Forms.TabPage();
+            this.main_tabPage_deleteEmpty = new System.Windows.Forms.TabPage();
+            this.main_tabPage_fileManipulation = new System.Windows.Forms.TabPage();
+            this.main_tabPage_mouseRecord = new System.Windows.Forms.TabPage();
+            this.main_tabPage_keyboardRecord = new System.Windows.Forms.TabPage();
+            this.main_tabPage_reactiveTest = new System.Windows.Forms.TabPage();
+            this.main_tabPage_steamThumbnailDeleter = new System.Windows.Forms.TabPage();
+            this.main_tabPage_settings = new System.Windows.Forms.TabPage();
+            this.tabControl_main.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabControl_main
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(13, 13);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(952, 539);
-            this.tabControl1.TabIndex = 0;
+            this.tabControl_main.Controls.Add(this.main_tabPage_aspectRatio);
+            this.tabControl_main.Controls.Add(this.main_tabPage_bpm);
+            this.tabControl_main.Controls.Add(this.main_tabPage_clickSpeed);
+            this.tabControl_main.Controls.Add(this.main_tabPage_clipboard);
+            this.tabControl_main.Controls.Add(this.main_tabPage_copyFinder);
+            this.tabControl_main.Controls.Add(this.main_tabPage_deleteEmpty);
+            this.tabControl_main.Controls.Add(this.main_tabPage_fileManipulation);
+            this.tabControl_main.Controls.Add(this.main_tabPage_keyboardRecord);
+            this.tabControl_main.Controls.Add(this.main_tabPage_mouseRecord);
+            this.tabControl_main.Controls.Add(this.main_tabPage_reactiveTest);
+            this.tabControl_main.Controls.Add(this.main_tabPage_steamThumbnailDeleter);
+            this.tabControl_main.Controls.Add(this.main_tabPage_settings);
+            this.tabControl_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl_main.Location = new System.Drawing.Point(0, 0);
+            this.tabControl_main.Multiline = true;
+            this.tabControl_main.Name = "tabControl_main";
+            this.tabControl_main.SelectedIndex = 0;
+            this.tabControl_main.Size = new System.Drawing.Size(1084, 562);
+            this.tabControl_main.TabIndex = 0;
             // 
-            // tabPage1
+            // main_tabPage_aspectRatio
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(944, 513);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.main_tabPage_aspectRatio.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_aspectRatio.Name = "main_tabPage_aspectRatio";
+            this.main_tabPage_aspectRatio.Padding = new System.Windows.Forms.Padding(3);
+            this.main_tabPage_aspectRatio.Size = new System.Drawing.Size(1076, 536);
+            this.main_tabPage_aspectRatio.TabIndex = 0;
+            this.main_tabPage_aspectRatio.Text = "Aspect Ratio";
+            this.main_tabPage_aspectRatio.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // main_tabPage_bpm
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.main_tabPage_bpm.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_bpm.Name = "main_tabPage_bpm";
+            this.main_tabPage_bpm.Padding = new System.Windows.Forms.Padding(3);
+            this.main_tabPage_bpm.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_bpm.TabIndex = 1;
+            this.main_tabPage_bpm.Text = "BPM Measurement";
+            this.main_tabPage_bpm.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_clickSpeed
+            // 
+            this.main_tabPage_clickSpeed.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_clickSpeed.Name = "main_tabPage_clickSpeed";
+            this.main_tabPage_clickSpeed.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_clickSpeed.TabIndex = 2;
+            this.main_tabPage_clickSpeed.Text = "ClickSpeed";
+            this.main_tabPage_clickSpeed.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_clipboard
+            // 
+            this.main_tabPage_clipboard.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_clipboard.Name = "main_tabPage_clipboard";
+            this.main_tabPage_clipboard.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_clipboard.TabIndex = 3;
+            this.main_tabPage_clipboard.Text = "Clipboard History";
+            this.main_tabPage_clipboard.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_copyFinder
+            // 
+            this.main_tabPage_copyFinder.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_copyFinder.Name = "main_tabPage_copyFinder";
+            this.main_tabPage_copyFinder.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_copyFinder.TabIndex = 4;
+            this.main_tabPage_copyFinder.Text = "Copy Finder";
+            this.main_tabPage_copyFinder.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_deleteEmpty
+            // 
+            this.main_tabPage_deleteEmpty.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_deleteEmpty.Name = "main_tabPage_deleteEmpty";
+            this.main_tabPage_deleteEmpty.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_deleteEmpty.TabIndex = 5;
+            this.main_tabPage_deleteEmpty.Text = "Delete Empty";
+            this.main_tabPage_deleteEmpty.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_fileManipulation
+            // 
+            this.main_tabPage_fileManipulation.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_fileManipulation.Name = "main_tabPage_fileManipulation";
+            this.main_tabPage_fileManipulation.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_fileManipulation.TabIndex = 6;
+            this.main_tabPage_fileManipulation.Text = "File Manipulation";
+            this.main_tabPage_fileManipulation.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_mouseRecord
+            // 
+            this.main_tabPage_mouseRecord.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_mouseRecord.Name = "main_tabPage_mouseRecord";
+            this.main_tabPage_mouseRecord.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_mouseRecord.TabIndex = 7;
+            this.main_tabPage_mouseRecord.Text = "MouseRecord";
+            this.main_tabPage_mouseRecord.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_keyboardRecord
+            // 
+            this.main_tabPage_keyboardRecord.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_keyboardRecord.Name = "main_tabPage_keyboardRecord";
+            this.main_tabPage_keyboardRecord.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_keyboardRecord.TabIndex = 8;
+            this.main_tabPage_keyboardRecord.Text = "Keyboard Record";
+            this.main_tabPage_keyboardRecord.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_reactiveTest
+            // 
+            this.main_tabPage_reactiveTest.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_reactiveTest.Name = "main_tabPage_reactiveTest";
+            this.main_tabPage_reactiveTest.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_reactiveTest.TabIndex = 9;
+            this.main_tabPage_reactiveTest.Text = "Reactive Test";
+            this.main_tabPage_reactiveTest.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_steamThumbnailDeleter
+            // 
+            this.main_tabPage_steamThumbnailDeleter.Location = new System.Drawing.Point(4, 22);
+            this.main_tabPage_steamThumbnailDeleter.Name = "main_tabPage_steamThumbnailDeleter";
+            this.main_tabPage_steamThumbnailDeleter.Size = new System.Drawing.Size(976, 536);
+            this.main_tabPage_steamThumbnailDeleter.TabIndex = 10;
+            this.main_tabPage_steamThumbnailDeleter.Text = "Steam Thumbnail Deleter";
+            this.main_tabPage_steamThumbnailDeleter.UseVisualStyleBackColor = true;
+            // 
+            // main_tabPage_settings
+            // 
+            this.main_tabPage_settings.Location = new System.Drawing.Point(4, 40);
+            this.main_tabPage_settings.Name = "main_tabPage_settings";
+            this.main_tabPage_settings.Size = new System.Drawing.Size(976, 518);
+            this.main_tabPage_settings.TabIndex = 11;
+            this.main_tabPage_settings.Text = "Settings";
+            this.main_tabPage_settings.UseVisualStyleBackColor = true;
             // 
             // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(977, 564);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(1084, 562);
+            this.Controls.Add(this.tabControl_main);
             this.Name = "GUI";
             this.Text = "GUI";
-            this.tabControl1.ResumeLayout(false);
+            this.tabControl_main.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl_main;
+        private System.Windows.Forms.TabPage main_tabPage_aspectRatio;
+        private System.Windows.Forms.TabPage main_tabPage_bpm;
+        private System.Windows.Forms.TabPage main_tabPage_clickSpeed;
+        private System.Windows.Forms.TabPage main_tabPage_clipboard;
+        private System.Windows.Forms.TabPage main_tabPage_copyFinder;
+        private System.Windows.Forms.TabPage main_tabPage_deleteEmpty;
+        private System.Windows.Forms.TabPage main_tabPage_fileManipulation;
+        private System.Windows.Forms.TabPage main_tabPage_keyboardRecord;
+        private System.Windows.Forms.TabPage main_tabPage_mouseRecord;
+        private System.Windows.Forms.TabPage main_tabPage_reactiveTest;
+        private System.Windows.Forms.TabPage main_tabPage_steamThumbnailDeleter;
+        private System.Windows.Forms.TabPage main_tabPage_settings;
     }
 }
