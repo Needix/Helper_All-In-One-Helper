539f8adb149aa59b5e3957b2ebef2bf701a785ac
Jonas Oeldemann <joeldem@mail.uni-paderborn.de>
2015-06-15 19:54:02 +0200

    - Created intial GUI - Created module structure

--- AllInOneHelper/Program.cs
+++ AllInOneHelper/Program.cs
@@ -12,7 +12,7 @@ namespace AllInOneHelper {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(src.GUI.GUI.getInstance);
         }
     }
 }
---
+++ AllInOneHelper/src/GUI/GUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AllInOneHelper.src.GUI {
+    public partial class GUI : Form {
+        //Constants
+
+        //Variables
+        private static GUI gui;
+        public static GUI getInstance {
+            get {
+                if(GUI.gui == null)
+                    GUI.gui = new GUI();
+                return GUI.gui; 
+            }
+        }
+
+        //Constructor
+        private GUI() {
+            InitializeComponent();
+
+            GUI.gui = this;
+        }
+
+        #region Events
+        protected override void OnClosing(CancelEventArgs e) {
+            base.OnClosing(e);
+
+        }
+        #endregion
+    }
+}
